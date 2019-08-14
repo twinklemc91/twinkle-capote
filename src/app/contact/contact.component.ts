1d07087d00907aeee5a5496c90b08a9d2eae3aab
twinkle <twinkle.capote@crossbridgecapital.com>
2019-08-14 13:00:46 +0800

    Animation on scroll

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,15 +9,21 @@ import {} from 'googlemaps';
 })
 
 export class ContactComponent implements OnInit {
-    @ViewChild('map') mapElement: any;
+    @ViewChild('map', {static:true}) mapElement: any;
     map: google.maps.Map;
 
+    public currentYear;
+
     ngOnInit() {
+        // Initialize Google Map
         const mapProperties = {
             center: new google.maps.LatLng(1.4010541, 103.9153112),
             zoom:12, scrollwheel: false, draggable: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
        };
        this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+
+       // initialize variables
+       this.currentYear = new Date().getFullYear();
     }
 }
